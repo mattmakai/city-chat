ca9b95e834bf116393395c6050ae5a2001f05214
Matt Makai <matthew.makai@gmail.com>
2015-11-20 16:57:24 -0800

    adding new required env vars for chat

--- chat/urls.py
+++ chat/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'/token$', views.token, name="token"),
     url(r'$', views.main, name="main"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
 
 
 def main(request):
     return render(request, 'chat.html', {})
+
+
+def token(request):
+    device_id = 'abc'
+    return 'ok'

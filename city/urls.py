8818d1bef1a4f2982fe4f7f09dd7c86b235046cc
Matt Makai <matthew.makai@gmail.com>
2015-11-24 21:35:58 -0800

    updating with chat template

--- city/urls.py
+++ city/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import url
+from django.conf.urls import include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('chat.urls', namespace='chat')),
 ]

7996bd9d77f56e8248666fe5ebfe619329074e7c
Matt Makai <matthew.makai@gmail.com>
2015-11-24 18:02:04 -0800

    adding csrf exempt to twilio post

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,9 @@
+import math
+
 from django.conf import settings
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 from twilio.access_token import AccessToken, IpMessagingGrant
 
@@ -9,8 +12,41 @@ def main(request):
     return render(request, 'chat.html', {})
 
 
+@csrf_exempt
 def chat_message(request):
-    return JsonResponse({'body': request.POST.get('body', '')})
+    return HttpResponse('200') #{'body': request.POST.get('body', '')})
+
+
+def _send_chats(latitude, longitude):
+    pass
+
+
+def distance_on_unit_sphere(lat1, long1, lat2, long2):
+    """
+        Gratefully obtained from this great blog post:
+        http://www.johndcook.com/blog/python_longitude_latitude/
+    """
+    # Convert latitude and longitude to
+    # spherical coordinates in radians.
+    degrees_to_radians = math.pi/180.0
+    # phi = 90 - latitude
+    phi1 = (90.0 - lat1)*degrees_to_radians
+    phi2 = (90.0 - lat2)*degrees_to_radians
+    # theta = longitude
+    theta1 = long1*degrees_to_radians
+    theta2 = long2*degrees_to_radians
+    # Compute spherical distance from spherical coordinates.
+    # For two locations in spherical coordinates
+    # (1, theta, phi) and (1, theta', phi')
+    # cosine( arc length ) =
+    #   sin phi sin phi' cos(theta-theta') + cos phi cos phi'
+    # distance = rho * arc length
+    cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) +
+           math.cos(phi1)*math.cos(phi2))
+    arc = math.acos( cos )
+    # Remember to multiply arc by the radius of the earth
+    # in your favorite set of units to get length.
+    return arc
 
 
 def token(request):

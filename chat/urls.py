8fcbdbd8162b22515d27962f9e12b8f6584e642b
Matt Makai <matthew.makai@gmail.com>
2015-11-24 20:44:38 -0800

    working through draft post

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'chat/message$', views.chat_message, name="chat_message"),
-    url(r'token$', views.token, name="token"),
-    url(r'$', views.main, name="main"),
+    url(r'$', views.chat, name="chat"),
 ]
--- city/urls.py
+++ city/urls.py
@@ -1,4 +1,4 @@
-"""neighbor URL Configuration
+"""city URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/dev/topics/http/urls/
@@ -14,11 +14,9 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
-from chat import urls as chat_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('chat.urls', namespace='chat')),
 ]

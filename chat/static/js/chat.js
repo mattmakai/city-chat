ae269d1d916af292c6c2b2a800030725f5a19ab6
Matt Makai <matthew.makai@gmail.com>
2015-11-25 11:10:23 -0800

    updating with latest code on chat side

--- chat/static/js/chat.js
+++ chat/static/js/chat.js
@@ -1,93 +1,109 @@
-$(function() {
-    // Get handle to the chat div 
-    var $chatWindow = $('#messages');
+// Get handle to the chat div 
+var $chatWindow = $('#messages');
 
-    // Manages the state of our access token we got from the server
-    var accessManager;
+// Manages the state of our access token we got from the server
+var accessManager;
 
-    // Our interface to the IP Messaging service
-    var messagingClient;
+// Our interface to the IP Messaging service
+var messagingClient;
 
-    // A handle to the "general" chat channel - the one and only channel we
-    // will have in this sample app
-    var generalChannel;
+// A handle to the chat channel based on location
+var generalChannel;
 
-    // The server will assign the client a random username - store that value
-    // here
-    var username;
+// The server will assign the client a username - stores that value
+var username;
 
-    // Helper function to print info messages to the chat window
-    function print(infoMessage, asHtml) {
-        var $msg = $('<div class="info">');
-        if (asHtml) {
-            $msg.html(infoMessage);
-        } else {
-            $msg.text(infoMessage);
-        }
-        $chatWindow.append($msg);
+// Helper function to print info messages to the chat window
+function print(infoMessage, asHtml) {
+    var $msg = $('<div class="info">');
+    if (asHtml) {
+        $msg.html(infoMessage);
+    } else {
+        $msg.text(infoMessage);
     }
+    $chatWindow.append($msg);
+}
 
-    // Helper function to print chat message to the chat window
-    function printMessage(fromUser, message) {
-        var $user = $('<span class="username">').text(fromUser + ':');
-        if (fromUser === username) {
-            $user.addClass('me');
-        }
-        var $message = $('<span class="message">').text(message);
-        var $container = $('<div class="message-container">');
-        $container.append($user).append($message);
-        $chatWindow.append($container);
+// Helper function to print chat message to the chat window
+function printMessage(fromUser, message) {
+    var $user = $('<span class="username">').text(fromUser + ':');
+    if (fromUser === username) {
+        $user.addClass('me');
     }
+    var $message = $('<span class="message">').text(message);
+    var $container = $('<div class="message-container">');
+    $container.append($user).append($message);
+    $chatWindow.append($container);
+}
 
-    // Alert the user they have been assigned a random username
-    print('Logging in...');
 
-    // Get an access token for the current user, passing a username (identity)
-    // and a device ID - for browser-based apps, we'll always just use the 
-    // value "browser"
-    $.getJSON('/token', {
-        identity: username,
-        device: 'browser'
-    }, function(data) {
-        // Alert the user they have been assigned a random username
-        username = data.identity;
-        print('You have been assigned a random username of: ' 
-            + '<span class="me">' + username + '</span>', true);
+function initialize() {
+  var mapCanvas = document.getElementById('map');
+  var latLng = new google.maps.LatLng(document.getElementById('lat').value, document.getElementById('long').value);
 
-        // Initialize the IP messaging client
-        accessManager = new Twilio.AccessManager(data.token);
-        messagingClient = new Twilio.IPMessaging.Client(accessManager);
+  var mapOptions = {
+    center: latLng,
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map,
+    title: 'Your location'
+  });
 
-        // Get the general chat channel, which is where all the messages are
-        // sent in this simple application
-        print('Attempting to join "general" chat channel...');
-        var promise = messagingClient.getChannelByUniqueName('general');
-        promise.then(function(channel) {
-            generalChannel = channel;
-            if (!generalChannel) {
-                // If it doesn't exist, let's create it
-                messagingClient.createChannel({
-                    uniqueName: 'general',
-                    friendlyName: 'General Chat Channel'
-                }).then(function(channel) {
-                    console.log('Created general channel:');
-                    console.log(channel);
-                    generalChannel = channel;
-                    setupChannel();
-                });
-            } else {
-                console.log('Found general channel:');
-                console.log(generalChannel);
-                setupChannel();
-            }
-        });
-    });
+  var latitude = $('#lat').val();
+  var longitude = $('#long').val();
+  var username;
+  var city;
 
-    // Set up channel after it has been found
-    function setupChannel() {
+  $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true', {}, function(locationData) {
+    console.log(locationData);
+    console.log(locationData.results[0].formatted_address);
+    // Alert the user they have been assigned an address
+    username = locationData.results[0]["formatted_address"];
+    city = locationData.results[0].address_components[3].long_name;
+    createChat();
+  });
+
+  function createChat() {
+    $.getJSON('/token', {identity: username, device: 'browser'}, function(data) {
+      print('It looks like you are near: ' 
+          + '<span class="me"><strong>' + username + '</strong></span>', true);
+
+      // Initialize the IP messaging client
+      accessManager = new Twilio.AccessManager(data.token);
+      messagingClient = new Twilio.IPMessaging.Client(accessManager);
+
+      // Get the general chat channel, which is where all the messages are
+      // sent in this simple application
+      var promise = messagingClient.getChannelByUniqueName('general');
+      promise.then(function(channel) {
+          generalChannel = channel;
+          if (!generalChannel) {
+              // If it doesn't exist, let's create it
+              messagingClient.createChannel({
+                  uniqueName: city,
+                  friendlyName: city
+              }).then(function(channel) {
+                  console.log('Created channel:');
+                  console.log(channel);
+                  generalChannel = city;
+                  setupChannel();
+              });
+          } else {
+              console.log('Found channel:');
+              console.log(generalChannel);
+              setupChannel();
+          }
+      });
+
+      // Set up channel after it has been found
+      function setupChannel() {
         // Join the general channel
         generalChannel.join().then(function(channel) {
-            print('Joined channel as ' 
+            print('Joined channel ' + city + ' as ' 
                 + '<span class="me">' + username + '</span>.', true);
         });
 
@@ -95,14 +111,28 @@ $(function() {
         generalChannel.on('messageAdded', function(message) {
             printMessage(message.author, message.body);
         });
-    }
+      }
 
-    // Send a new message to the general channel
-    var $input = $('#chat-input');
-    $input.on('keydown', function(e) {
+      // Send a new message to the general channel
+      var $input = $('#chat-input');
+      $input.on('keydown', function(e) {
         if (e.keyCode == 13) {
             generalChannel.sendMessage($input.val())
             $input.val('');
         }
+      });
     });
-});
+  }
+}
+
+function positionFound(position) {
+  document.getElementById('lat').value = position.coords.latitude;
+  document.getElementById('long').value = position.coords.longitude;
+  initialize();
+}
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(positionFound);
+} else {
+  alert('It appears that required geolocation is not enabled in your browser.');
+}

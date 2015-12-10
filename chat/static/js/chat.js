230a974ddbaa439d5a7da61b1431f8c97a33e32f
Matt Makai <matthew.makai@gmail.com>
2015-12-10 11:54:34 -0700

    cleaning up js a bit

--- chat/static/js/chat.js
+++ chat/static/js/chat.js
@@ -29,6 +29,7 @@ function printMessage(fromUser, message) {
     $chatWindow.append($container);
 }
 
+// creates the map based on user's browser location 
 function drawMap() {
   var mapCanvas = document.getElementById('map');
   var latLng = new google.maps.LatLng(document.getElementById('lat').value, document.getElementById('long').value);
@@ -46,8 +47,14 @@ function drawMap() {
   });
 }
 
+
 function initialize() {
   drawMap();
+  chatBasedOnCity();
+}
+
+
+function chatBasedOnCity() {
   var latitude = $('#lat').val();
   var longitude = $('#long').val();
   $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true', {}, function(locationData) {
@@ -56,62 +63,63 @@ function initialize() {
     city = locationData.results[0].address_components[3].long_name;
     createChat();
   });
+}
 
-  function createChat() {
-    $.getJSON('/token', {identity: username, device: 'browser'}, function(data) {
-      print('It looks like you are near: ' 
-          + '<span class="me"><strong>' + userLocation + '</strong></span>', true);
 
-      accessManager = new Twilio.AccessManager(data.token);
-      messagingClient = new Twilio.IPMessaging.Client(accessManager);
+function createChat() {
+  $.getJSON('/token', {identity: username, device: 'browser'}, function(data) {
+    print('It looks like you are near: ' 
+        + '<span class="me"><strong>' + userLocation + '</strong></span>', true);
 
-      var promise = messagingClient.getChannelByUniqueName(city);
-      promise.then(function(channel) {
-          console.log(channel);
-          generalChannel = channel;
-          if (!generalChannel) {
-              // If it doesn't exist, let's create it
-              messagingClient.createChannel({
-                  uniqueName: city,
-                  friendlyName: city
-              }).then(function(channel) {
-                  console.log('Created channel:');
-                  console.log(channel);
-                  generalChannel = channel;
-                  setupChannel();
-              });
-          } else {
-              console.log('Found channel:');
-              console.log(generalChannel);
-              setupChannel();
-          }
-      });
+    accessManager = new Twilio.AccessManager(data.token);
+    messagingClient = new Twilio.IPMessaging.Client(accessManager);
 
-    });
-
-    // Set up channel after it has been found
-    function setupChannel() {
-        // Join the general channel
-        generalChannel.join().then(function(channel) {
-            print('Joined channel "' + channel.uniqueName + '" as ' 
-                + '<span class="me">' + username + '</span>.', true);
-        });
-
-        // Listen for new messages sent to the channel
-        generalChannel.on('messageAdded', function(message) {
-            printMessage(message.author, message.body);
-        });
-    }
-
-    // Send a new message to the general channel
-    var $input = $('#chat-input');
-    $input.on('keydown', function(e) {
-        if (e.keyCode == 13) {
-            generalChannel.sendMessage($input.val())
-            $input.val('');
+    var promise = messagingClient.getChannelByUniqueName(city);
+    promise.then(function(channel) {
+        console.log(channel);
+        generalChannel = channel;
+        if (!generalChannel) {
+            // If it doesn't exist, let's create it
+            messagingClient.createChannel({
+                uniqueName: city,
+                friendlyName: city
+            }).then(function(channel) {
+                console.log('Created channel:');
+                console.log(channel);
+                generalChannel = channel;
+                setupChannel();
+            });
+        } else {
+            console.log('Found channel:');
+            console.log(generalChannel);
+            setupChannel();
         }
     });
+
+  });
+
+  // Set up channel after it has been found
+  function setupChannel() {
+      // Join the general channel
+      generalChannel.join().then(function(channel) {
+          print('Joined channel "' + channel.uniqueName + '" as ' 
+              + '<span class="me">' + username + '</span>.', true);
+      });
+
+      // Listen for new messages sent to the channel
+      generalChannel.on('messageAdded', function(message) {
+          printMessage(message.author, message.body);
+      });
   }
+
+  // Send a new message to the general channel
+  var $input = $('#chat-input');
+  $input.on('keydown', function(e) {
+      if (e.keyCode == 13) {
+          generalChannel.sendMessage($input.val())
+          $input.val('');
+      }
+  });
 }
 
 function positionFound(position) {

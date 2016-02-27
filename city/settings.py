74a1d142d91795c2382d2fbac7efe59f985bec95
Matt Makai <makaimc@gmail.com>
2016-02-27 08:10:49 -0800

    Update settings.py for STATIC_ROOT
    Update settings.py for STATIC_ROOT based on issue #1

--- city/settings.py
+++ city/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', None)
